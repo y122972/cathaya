6f9351ecf03c53bf3f184d3161c3e71ca0b4b8ee
y122972 <y122972@gmail.com>
2023-11-15 07:10:04 +0000

    Initial page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center text-[100px] font-[Consolas] font-semibold">
+      <div className="inline-block bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-transparent bg-clip-text">
+        Cathaya
+      </div>
+    </main>
+  );
+}
